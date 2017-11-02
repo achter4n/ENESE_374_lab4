39c816971df4b0fa7ca6e12a790e15411e510248
achter4n <achter4n@uregina.ca>
2017-11-02 17:41:58 -0600

    Tentatively finalized Personal and Corporate customer classes

--- src/CorporateCustomer.java
+++ src/CorporateCustomer.java
@@ -1,3 +1,9 @@
 public class CorporateCustomer extends Customer{
-	private String contactName;
+	private String contactName;		//contact name for corporate customer
+	
+	//constructor
+	public CorporateCustomer(String name, String address, float creditRating, float discountRating, String contactName) {
+		super(name, address, creditRating, discountRating);
+		this.contactName = contactName;
+	}
 }
--- src/PersonalCustomer.java
+++ src/PersonalCustomer.java
@@ -1,3 +1,9 @@
 public class PersonalCustomer extends Customer{
-	private int creditCardNumber;
+	private int creditCardNumber;	//customers credit card number
+	
+	//constructor
+	public PersonalCustomer(String name, String address, float creditRating, float discountRating, int creditCardNumber) {
+		super(name, address, creditRating, discountRating);
+		this.creditCardNumber = creditCardNumber;
+	}
 }

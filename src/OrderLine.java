090a144ab596f6e86d92e9297396369b77072187
achter4n <achter4n@uregina.ca>
2017-11-02 17:51:42 -0600

    Tentatively finalized the OrderLine class

--- src/OrderLine.java
+++ src/OrderLine.java
@@ -1,22 +1,22 @@
 public class OrderLine {
-	private int quantity;
-	private double price;
-	private Product product;
+	private int quantity;		//quantity of products
+	private double price;		//price of order
+	private Product product;	//a product
 	
+	//constructor
 	OrderLine(Product product, int quantity, double price) {
 		this.product = product;
 		this.quantity = quantity;
 		this.price = price;
 	}
 	
+	//getters
 	public Product getProduct() {
 		return product;
 	}
-	
 	public int getQuantity() {
 		return quantity;
 	}
-	
 	public double getPrice() {
 		return price;
 	}

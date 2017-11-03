6dd0d794b376f67621cac6b57fa882f831ce1acd
achter4n <achter4n@uregina.ca>
2017-11-02 20:56:44 -0600

    Tentatively finalized Order class

--- src/Order.java
+++ src/Order.java
@@ -1,15 +1,47 @@
-import java.util.Date;
+import java.util.*;
 
 public class Order {
-	private Date dateReceived;
-	private Double price;
-	private int orderID;
+	private Date dateReceived;							//date order was received on
+	private double price;								//price of the order
+	private int orderID;								//ID for the order
+	private Customer customer;							//customer making the order
+	public ArrayList<OrderLine> ol = new ArrayList<OrderLine>();	//list of products in the order line
 	
+	//constructor
+	public Order(Customer customer) {
+		dateReceived = new Date();
+		price = 0;
+		orderID = 1;
+		this.customer = customer;
+	}
+	
+	//getter for date
 	public Date getDateReceived() {
 		return dateReceived;
 	}
-	
-	public Double calculatePrice() {
+
+	//calculates the order price
+	public double calculatePrice() {
+		Iterator<OrderLine> iterator = ol.iterator();	//iterator to iterate through the list
+		OrderLine currentOrder;							//current order in the line
+		Product currentProduct;							//current product being looked at
+		double currentPrice;							//price of the current product
+		
+		//while not at the end of the list
+		while(iterator.hasNext()) {
+			currentOrder = iterator.next();
+			currentProduct = currentOrder.getProduct();
+			currentPrice = currentProduct.getPrice();
+			currentPrice *= currentOrder.getQuantity();
+			price += currentPrice;
+		}
+		price -= price * customer.getDiscountRating();	//applied customer's discount
 		return price;
+	}
+	
+	//adds a new item to the order
+	public void addItem(Product product, int quantity) {
+		OrderLine newOrder = new OrderLine(product, quantity, product.getPrice());
+		ol.add(newOrder);
 	}
 }

82b6589c7f56048c9bf90fbdc4168bc1639d3b09
achter4n <achter4n@uregina.ca>
2017-11-02 23:07:57 -0600

    Minor editting and added comments

--- src/OrderApplication.java
+++ src/OrderApplication.java
@@ -1,27 +1,30 @@
-
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class OrderApplication {
-	private static ArrayList<Product> products = new ArrayList<Product>();
+	private static ArrayList<Product> catalogue = new ArrayList<Product>();	//catalogue of products
 	
 	public static void main(String[] args) throws IOException {
 		OrderApplication oa = new OrderApplication();
 		Customer customer = new Customer("Nic Achter", "123 Road Ave", 0.0f, 0.0f);
 		Order order = new Order(customer);
 		
-		if (createOrder(order)) {
+		//if an order can be created, print the price of the order
+		if (oa.createOrder(order)) {
 			System.out.printf("The price of the order is $%.1f\n", order.calculatePrice());
 		}
 	}
 	
-	public static boolean createOrder(Order order) throws IOException {
-		boolean open = importCatalogue();
+	//creates a new order
+	public boolean createOrder(Order order) throws IOException {
+		boolean open = importCatalogue();	//boolean to ensure that 
 
+		//if the file opened properly
 		if (open) {
-			Iterator<Product> iterator = products.iterator();
+			Iterator<Product> iterator = catalogue.iterator();	//iterator
 
+			//while not the end of the list, add item
 			while (iterator.hasNext()) {
 				Product product = iterator.next();
 				order.addItem(product, 1);
@@ -30,17 +33,20 @@ public class OrderApplication {
 		return open;
 	}
 
-	private static boolean importCatalogue() throws IOException {
-		boolean open = false;
-		Scanner in = null;
+	//get the catalogue of items from a text file
+	private boolean importCatalogue() throws IOException {
+		boolean open = false;				//boolean for if the file opened
+		Scanner in = null;					//input scanner
 		try {
-			in = new Scanner(Paths.get("ProductCatalogue.txt"));
+			in = new Scanner(Paths.get("ProductCatalogue.txt"));	//grab text from file
+			//while not end of file
 			while (in.hasNextLine()) {
 				String[] data = in.nextLine().split(",");
 				Product product = new Product(data[0], Double.parseDouble(data[1]), Integer.parseInt(data[2]));		
-				products.add(product);
+				catalogue.add(product);
 			}
 		}finally {
+			//if the input isn't empty, close file and set open to true
 			if (in != null) {
 				in.close();
 				open = true;
